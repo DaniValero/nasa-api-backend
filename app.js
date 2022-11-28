c64361ace6ceca228dd692e885685f59efe371c9
Dani Valero <danivalero98@hotmail.com>
2022-11-28 16:02:11 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+
+// GET para obtener nombre y masa de todos aquellos meteoritos cuya masa sea igual o superior a una masa (gr) dada (con query parameters)​
+
+app.get('/astronomy/landings', (req, res) => {
+
+    let query = ""
+    let error = {}
+
+    
+
+
+
+
+}) 
+
+app.listen(3000, console.log("Dios le da sus peores batallas a sus mejores guerreros"))
