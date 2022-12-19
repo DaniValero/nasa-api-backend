f215002ef5bc99fc5d37c91dced641820c4604b7
Dani Valero <danivalero98@hotmail.com>
2022-12-19 11:08:40 +0100

    Arreglados carpetas para despliegue

--- src/startup/db.js
+++ src/startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = function() {
-    mongoose.connect('mongodb://127.0.0.1:27017/nasa-api')
+    mongoose.connect(process.env.MONGO_URI)
         .then(() => console.log("Tenemos conexión con la db..."))
         .catch(() => console.log("ERROR FATAL: ", err))   
 }
