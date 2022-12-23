ac5e01dc477dc58034e3ecb8d6f268750be800d3
Dani Valero <danivalero98@hotmail.com>
2022-12-23 09:29:41 +0100

    CORS implementado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.json())
 require('dotenv').config()
+
+
+
 
 const landings = require('./routes/landings')
 const neas = require('./routes/neas')
